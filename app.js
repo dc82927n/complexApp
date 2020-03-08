4af566515b2186067e30fc23b2a42c1687ae783b
David <dc82927n@gmail.com>
2020-03-08 17:09:22 -0400

    need to delete previous version to install new json package to revert to old revision + added some validators to the signup app. currently a working model

--- app.js
+++ app.js
@@ -1,53 +1,15 @@
-
 const express = require('express')
 const app = express()
 
-/* we test the router */
-/* we can use any name but for the const but we going to use require to use the newly create router file */
 const router = require('./router')
-console.log(router)
 
-// using the base and use the router file we created. 
-app.use('/', router) 
-// using the css
-app.use(express.static('public'))
-// the first parameter because it's an express option 
-// the second argeument can be anything but because we name our folder views 
-app.set('views', 'views')
-// this will tell the view engine which engine to use. 
-app.set('view engine', 'ejs')
-
-// this will tell the new router file we just set up to use
-// we going to use the .use method and it's gonna take in two argument
-// the first argument is / which is going to be our base of our site and then
-// the second argument is router 
-app.use("/", router)
-
-/* boiler for summiting data on the web and sending some json data*/
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
+app.use(express.static('public'))
+app.set('views', 'views')
+app.set('view engine', 'ejs')
+
+app.use('/', router)
 
 app.listen(3000)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,33 +1,23 @@
+const User = require('../models/User')
 
-// when the node see this it will know that to do with it. this what will be called when they are the base location. 
 exports.login = function() {
 
 }
 
-
-
+exports.logout = function() {
+  
+}
 
 exports.register = function(req, res) {
-    console.log("hello this is a test if we can access the data")
-    res.send('thank you for registering')
+  let user = new User(req.body)
+  user.register()
+  if (user.errors.length) {
+    res.send(user.errors)
+  } else {
+    res.send("Congrats, there are no errors.")
+  }
 }
-
-
 
 exports.home = function(req, res) {
-    res.render('home-guest')
+  res.render('home-guest')
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
